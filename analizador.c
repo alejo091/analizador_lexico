e1e3b50a7d58e3e4e96e7e8ddd337665c716b06d
alejo091 <jacastroo@unal.edu.co>
2018-03-19 20:45:24 -0500

    a

--- analizador.c
+++ analizador.c
@@ -1,95 +1,153 @@
 /*
 *  Autor: Alejandro Castro
-*  
+*
 *  Temas Avanzados en Lenguajes de Programacion
 *  2018-1
 */
 
 #include <stdio.h>
 #include <stdint.h>
-#include "reservados_especiales.h"
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
 
 #define FIPATH "/home/alejandro/Documents/Maestria/S3/Lenguajes_Programacion/taller1/testCases/0/in01.txt"
-#define	MAX_WORD_SIZE	255
+#define MAX_PAL_RESER		15
+#define MAX_WORD_SIZE   10
 
-FILE *filein;
-FILE *fileout;
+char tokens_esp[25][25] = { "token_mayor","token_menor","token_mas","token_div",
+                    "token_menos","token_mul","token_mod","token_mod",
+                    "token_pot","token_not","token_assign","token_par_izq",
+                    "token_par_der","token_llave_izq","token_llave_der",
+                    "token_cor_izq","token_cor_der","token_coma","token_dosp"
+                    "token_point"};
 
-uint16_t fila;
-uint16_t columna;
-char current_word[MAX_WORD_SIZE];
+char pal_res[MAX_PAL_RESER][MAX_WORD_SIZE] = {"true","false","nil","if","else","while",
+                		                        "log","for","in","funcion","end"
+                		                        "retorno","importar","desde","todo"};
 
+uint8_t pos;
 
+uint8_t palabra_reservada(char buffer[])
+{
 
+    uint8_t flag = 0;
 
+    for(pos = 0; pos < MAX_PAL_RESER; ++pos)
+    {
+        if(strcmp(pal_res[pos], buffer) == 0)
+        {
+            flag = 1;
+            break;
+        }
+    }
 
+    return flag;
+}
 
-
-
-
-
-
-
-
-
-
-
-/*
 int main()
 {
-  filein = fopen("in01.txt","r");
-  fileout = fopen("out.txt","w+");
+    char operadores_especiales[] = "><+/-*%%^!=(){}[],:.";
 
-  int chat_read;
-  uint8_t letter = 0;
-  uint8_t palres = 0;
-  uint8_t char_ok = 0;
+    FILE *filein;
+    FILE *fileout;
+    filein = fopen("in01.txt","r");
+    fileout = fopen("out.txt","w+");
 
-  while ((chat_read = getc(filein)) != EOF)
-  {
-    char_ok = 0;
-    palres = 0;
+    char chat_read;
+    uint8_t i;
+    uint8_t j;
+    uint8_t char_ok;
+    uint8_t char_esp;
+    uint16_t fila = 1;
+    uint16_t columna = 1;
+    char buffer[50];
 
-    current_word[letter] = chat_read;
+    if(filein == NULL){
+        printf(">>>>No se encontro el archivo a abrir\n");
+        exit(0);
+    }
 
-  
-    for(uint8_t ches = 0; ches < MAX_CHAR_ESP; ++ches)
+    while((chat_read = fgetc(filein)) != EOF)
     {
-      if(current_word[letter] == (intptr_t)char_esp[ches])
+      while(chat_read == '#')
       {
-        char_ok = 1;
-        break;
+        ++fila;
+        chat_read = fgetc(filein);
       }
-    }
-
-    for(uint8_t chesdo = 0; chesdo < MAX_CHAR_ESP_DOB; ++chesdo)
-    {
-      if(current_word == char_esp_dob[chesdo])
+      char_ok = 1;
+      char_esp = 0;
+      /*
+      * Look for [a-z], [A-Z] or [0-9] characters
+      */
+      if(isalnum(chat_read))
       {
-        char_ok = 1;
-        break;
+        buffer[j++] = chat_read;
       }
+      else
+      {
+
+        /*
+        * Look for any special character
+        */
+        for(i = 0; i < 20; ++i)
+        {
+          if(chat_read == operadores_especiales[i])
+          {
+            char_esp = 1;
+            break;
+          }
+        }
+
+        if(char_esp == 1)
+        {
+          if(palabra_reservada(buffer) == 1)
+          {
+            buffer[j]='\0';
+            fprintf(fileout, "<%s,%d,%d>\n", pal_res[pos], fila, columna-j);
+            for(uint8_t i = 0;i < sizeof(buffer); ++i)buffer[i]='\0';
+            j = 0;
+          }
+          fprintf(fileout, "<%s,%d,%d>\n", tokens_esp[i], fila, columna);
+        }
+
+        if(chat_read == 0x0A || chat_read == 0x0D || chat_read == 0x20 || chat_read == 0x09)
+        {
+          if(palabra_reservada(buffer) == 1)
+          {
+            buffer[j]='\0';
+            fprintf(fileout, "<%s,%d,%d>\n", pal_res[pos], fila, columna-j);
+            for(uint8_t i = 0;i < sizeof(buffer); ++i)buffer[i]='\0';
+            j = 0;
+          }
+          if(chat_read == 0x0A)
+          {
+            ++fila;
+            columna = 0;
+          }
+        }
+        else
+        {
+          if(char_esp == 0)
+          {
+            char_ok = 0;
+          }
+        }
+      }
+      if(char_ok == 0)
+      {
+        printf("\n--- char read: %d",chat_read);
+        printf("\n>>>>Error lexico(linea:%d,posicion:%d)\n",fila,columna);
+        return 1;
+      }
+      else
+      {
+        putchar(chat_read);
+      }
+      ++columna;
     }
 
-    if(char_ok == 0)
-    {
-      printf(">>>>Error lexico(linea:%d,posicion:%d)\n",1,2);
-      return 1;
-    }
+    fclose(filein);
 
-    putchar(current_word[letter]);
-    ++letter;   
-  }
-
-  for(uint8_t i = 0; i < 100; ++i) putchar(current_word[i]);
-
-  fprintf(fileout, "This is testing for fprintf...\n");
-  fputs("This is testing for fputs...\n", fileout);
-
-  fclose(filein);
-  fclose(fileout);
-
-  return 0;
+    return 0;
 }
-*/
-
--- reservados_especiales.h
+++ reservados_especiales.h
@@ -1,6 +1,6 @@
 /*
 *  Autor: Alejandro Castro
-*  
+*
 *  Temas Avanzados en Lenguajes de Programacion
 *  2018-1
 */
@@ -12,12 +12,12 @@
 const char *palabras_reservadas[MAX_PAL_SERVER] = {
 		"true","false","nil","if","else","while",
 		"log","for","in","funcion","end"
-		"retorno","importar","desde","todo","."
+		"retorno","importar","desde","todo"
 		};
 
 const char *char_esp[MAX_CHAR_ESP] = {
 		">","<","+","-","*","/","%%","^","!",
-		"=","(",")","{","}","[","]",",",":"
+		"=","(",")","{","}","[","]",",",":","."
 		};
 
 const char *char_esp_dob[MAX_CHAR_ESP_DOB] = {
